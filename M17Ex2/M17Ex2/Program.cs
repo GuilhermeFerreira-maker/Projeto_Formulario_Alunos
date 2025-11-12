51f6a54b4757c1a0b852a6091d64a5a0e8fa93f8
Guilherme Gomes Ferreira 12 - Prof - 3F <al.917286@aeaav.pt>
2025-11-12 17:53:32 +0000

    Ex2

---
+++ M17Ex2/M17Ex2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M17Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Incío de Programa");
+                Console.WriteLine("Escolher 8 números");
+                for (int i = 0; i < 8; i++)
+                {
+                    int N = int.Parse(Console.ReadLine());
+                }
+
+            }
+            catch (Exception Num)
+            {
+                Console.WriteLine("Erro: " + Num.Message);
+            }
+        }
+    }
+}
